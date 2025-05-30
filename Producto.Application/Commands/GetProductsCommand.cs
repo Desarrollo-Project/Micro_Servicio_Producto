f9914c3f01e2c0eda987f08cdc3f48e1084d58ca
AG72 <acuario7203@gmail.com>
2025-05-29 21:45:09 -0400

    FEAT[#SCRUM-36]: Consultar_Producto

---
+++ Producto.Application/Commands/GetProductsCommand.cs
@@ -0,0 +1,12 @@
+﻿// En Producto.Application.Commands/GetProductsCommand.cs
+using MediatR;
+using Producto.Domain.Aggregates; // Para Product
+using System.Collections.Generic;
+
+namespace Producto.Application.Commands
+{
+    public class GetProductsCommand : IRequest<IEnumerable<Product>> // Correcto, espera Product
+    {
+        public GetProductsCommand() { }
+    }
+}
